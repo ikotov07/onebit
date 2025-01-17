ef1a8d29127d4d22d4390f1c00839d0c9fbe00c8
ikotov07 <0ikotov07@gmail.com>
2025-01-17 13:41:01 +0200

    backdrop-filter:header

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,10 +5,35 @@ $(document).ready(function(){
   });
 })
 
+const time = 0
+let greeting;
+
+if (time < 4) {
+    greeting = "Good night";
+} else if (time < 12) {
+    greeting = "Good morning";
+} else if (time < 16) {
+    greeting = "Good day";
+} else if (time < 24) {
+    greeting = "Good evening";
+} 
+
+console.log(greeting);
+
+
+window.addEventListener("scroll", function() {
+  const header = document.querySelector("header");
+  if (window.scrollY > header.offsetTop) {
+      header.classList.add("fixed");
+  } else {
+      header.classList.remove("fixed");
+  }
+});
+
+
 const btnHistoryBack = document.querySelector('#history-back');
 
 btnHistoryBack.addEventListener('click', ()=> {
   window.history.back();
   })
-
 
